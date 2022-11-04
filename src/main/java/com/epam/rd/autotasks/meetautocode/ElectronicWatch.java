83b3544600a3f17e3c5570b4e0792c9887c67d34
Anastasiya Sakharuk <sakharuk333@gmail.com>
2022-11-04 20:14:52 +0300

    Electronic Watch

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -4,9 +4,19 @@ import java.util.Scanner;
 
 public class ElectronicWatch {
     public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
-
+        int sec, min, chas;
+        sec=seconds % 60; // отделили секунды
+        int m=(seconds - sec) / 60;
+        min=m%60;      // отделили минуты
+        int h=(m-min)/60; // знаем сколько часов осталось
+        chas=h%24; // отделили часы в сутках
+        stringBuilder = stringBuilder.append(chas + ":");
+        stringBuilder = min > 9 ? stringBuilder.append(min + ":") : stringBuilder.append("0" + min + ":");
+        stringBuilder = sec > 9 ? stringBuilder.append(sec) : stringBuilder.append("0" + sec);
+        System.out.println(stringBuilder);
 
     }
 }
